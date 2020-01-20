a622a3f38964a794ab4990b9406c884ae49da613
Jonathan Riecke <jonathanriecke@gmail.com>
2020-01-20 01:19:43 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,8 +9,10 @@ end
 def print_first_directors_movie_titles
   movies = directors_database[0][:movies]
   i = 0
-  while i < movies.length do
-    puts movies[i][:title]
+  
+  while i < directors_database.length do
+    column_index = 0
+    while column_index < directors_database[i].length
     i += 1
   end
 end
